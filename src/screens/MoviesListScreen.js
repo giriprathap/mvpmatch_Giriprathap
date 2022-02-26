24766a844bfca6d03daa78fd035cd7c175382d9a
giriprathap <giriprathapraju.v@gmail.com>
2022-02-27 00:14:00 +0530

    Initial Commit

---
+++ src/screens/MoviesListScreen.js
@@ -0,0 +1,12 @@
+import {React} from 'react';
+import { SafeAreaView,FlatList } from 'react-native';
+
+const MoviesListScreen = () => {
+    return(
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    );
+}
+
+export default MoviesListScreen;
